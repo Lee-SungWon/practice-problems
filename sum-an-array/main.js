d1480a5160dc4f8e97abd5f79f1203be083cc610
Chris SW Lee <sungwlee8@gmail.com>
2020-07-28 10:50:30 -0700

    sum-an-array

--- sum-an-array/main.js
+++ sum-an-array/main.js
@@ -1,6 +1,6 @@
-var sum = 0;
 
 function sumArray(array) {
+  var sum = 0; // this variable should be declared locally!!! Otherwise, if you run the var multiple times, it's going to add on top of the previous sum.
   for (i = 0; i < array.length; i++) {
     sum = sum + array[i];
   }
