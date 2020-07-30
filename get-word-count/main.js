43bb36e39fa407aebf39e5f29679d8f294a19cde
Chris SW Lee <sungwlee8@gmail.com>
2020-07-30 10:57:24 -0700

    create-repeating-letters

--- get-word-count/main.js
+++ get-word-count/main.js
@@ -2,8 +2,12 @@ function getWordCount (str) {
   var spaces = 0
   for (var i = 0; i <= str.length; i++) {
     if (str[i] === ' ') {
-      spaces += 1;
+      spaces += 1; // alternative: spaces++
     }
   }
   return spaces + 1;
 }
+// with split method:
+// function getWordCount2(str) {
+// retrun str.split(' ').length;
+// }
