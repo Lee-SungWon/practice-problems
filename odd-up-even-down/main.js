e2d3cb3162d239f6dc18fb5f3de9a2a2dbfe9d9c
Chris SW Lee <sungwlee8@gmail.com>
2020-08-12 10:36:56 -0700

    notes added

--- odd-up-even-down/main.js
+++ odd-up-even-down/main.js
@@ -9,11 +9,15 @@ function oddUpEvenDown(array) {
   var newArr = []
   for (var i = 0; i < array.length; i++) {
     if (array[i] % 2 === 1) {
-      array[i] += 1;
+      array[i] += 1;  //alternative: result.push(array[i] - 1) //can't increment or decrement because that will affect the original
     } else {
-      array[i] -= 1;
+      array[i] -= 1; //alternative: result.push(array[i] + 1)
     }
     newArr.push(array[i]);
   }
   return newArr;
 }
+
+/*
+array = array.slice() // make a copy of it through slice method & the new array it directly.  can increment or decrement now.
+*/
