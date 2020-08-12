ff2c4a45e77ffe5d515aa14a747e38c92f42aba7
Chris SW Lee <sungwlee8@gmail.com>
2020-08-12 10:20:20 -0700

    odd-up-even-down

---
+++ odd-up-even-down/main.js
@@ -0,0 +1,19 @@
+/*
+takes in an array of numeric values and increments any odd values by 1, and decrements any even values by 1
+1. go through each item in an array
+2. if the value of the item is odd, increment by one
+3. if the value of the item is even, decrement by one
+*/
+
+function oddUpEvenDown(array) {
+  var newArr = []
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      array[i] += 1;
+    } else {
+      array[i] -= 1;
+    }
+    newArr.push(array[i]);
+  }
+  return newArr;
+}
