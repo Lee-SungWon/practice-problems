721d8b023ec1faaf2eef3dda684cde7ffb88b430
Chris SW Lee <sungwlee8@gmail.com>
2020-08-26 10:23:48 -0700

    account-number-protector

--- account-number-protector/main.js
+++ account-number-protector/main.js
@@ -6,3 +6,20 @@ function accountNumberProtector(cardNumberString) {
   }
   return lastFourDigits;
 }
+
+/* alternatives
+
+const accountNumberProtector = cardNumberString => {
+  return `**${cardNumberString[12]}${cardNumberString[13]}${cardNumberString[14]}${cardNumberString[15]}`
+}
+
+function accountNumberProtector(cardNumberString) {
+  return '**' + cardNumberString.substring(12);
+}
+
+function accountNumberProtector(cardNumberString) {
+  return `**${cardNumberString
+    .split("")
+    .slice(cardNumberString.length - 4, cardNumberString.length)
+    .join("")}`;
+}
